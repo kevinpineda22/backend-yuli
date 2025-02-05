3fc7f4d9161771d229a7bbda148e255e01e3df80
johan <juanmerkahorro@gmail.com>
2025-02-05 10:44:27 -0500

    despliegue de backend a vercel

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -4,9 +4,9 @@ const { crearFormulario, respuestaDirector, respuestaGerencia, obtenerHistorial 
 
 const router = express.Router();
 
-router.post('/forms', crearFormulario);
-router.put('/forms/:workflow_id/director', respuestaDirector);
-router.put('/forms/:workflow_id/gerencia', respuestaGerencia);
-router.get('/forms/:workflow_id', obtenerHistorial);
+router.post('/yuli', crearFormulario);
+router.put('/yuli/:workflow_id/director', respuestaDirector);
+router.put('/yuli/:workflow_id/gerencia', respuestaGerencia);
+router.get('/yuli/:workflow_id', obtenerHistorial);
 
 module.exports = router;
