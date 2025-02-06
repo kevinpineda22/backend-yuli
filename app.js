d8a0e0b6c5664a95fbfc42513e7b931fca09a139
Juan Manuel <juanmerkahorro@gmail.com>
2025-02-06 09:41:20 -0500

    backend al pelo

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import supabase from './supabaseCliente';
-import formRoutes from './routes/formRoutes';
-
-dotenv.config();
+import cors from 'cors';
+import formRoutes from './routes/formRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configuración de CORS
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://www.merkahorro.com/'],  // Permitir solicitudes desde tu frontend en desarrollo y producción
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+  credentials: true  // Permitir el envío de cookies o cabeceras de autenticación si es necesario
+}));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware para inyectar supabase en req
-app.use((req, res, next) => {
-  req.supabase = supabase;
-  next();
-});
-
-// Usamos las rutas bajo el path /api
 app.use('/api', formRoutes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
-// Ruta de prueba para verificar que el servidor está funcionando
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'el ternero se crió correctamente' });
 });
