a250ae432ab878ffd0dde419bcc30010d8f96aac
isazaaa <isazamanuel04@gmail.com>
2026-03-31 09:59:27 -0500

    fix: add Cache-Control and Pragma to CORS allowedHeaders

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
     return callback(null, true);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
   credentials: true,
 }));
 
